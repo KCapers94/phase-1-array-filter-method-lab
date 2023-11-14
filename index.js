a8be47d50ab2ab447c9050fd48bce77bd73eda29
Keith Capers <K.Capers94@gmail.com>
2023-11-14 12:32:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(names, string) {
+    return names.filter((name) => name.toUpperCase() === string.toUpperCase())
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+console.log(findMatching(drivers, 'Bobby'))
+
+
+function fuzzyMatch(names, string) {
+    return names.filter((name) => name.startsWith(string)) 
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => driver.name ===  string)
+}
